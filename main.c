e2c6fd49bcd0c07aa7b6775ea0ab51f911c9cbc3
NishaantKrSingh <nishaantkrsingh@gmail.com>
2024-04-15 22:22:34 +0530

    Removed unnecessary files made for testing purposes, cleaned code and added proper indentation etc to the main.c file

--- main.c
+++ main.c
@@ -7,29 +7,19 @@
 int main() {
   struct stat attr;
 
-  // Get the file attributes and check for errors
- 
-  while (1)
-  {
-     if (stat("content.md", &attr) == -1) {
-    perror("Error in stat");
-    return 1;
+  while (1) {
+    if (stat("content.md", &attr) == -1) {
+      perror("Error in stat");
+      return 1;
   }
-
-  // Convert the time of the last modification to the contents of the file to a character string.
   char *ctime_str = ctime(&attr.st_mtime);
     char str[100];
-  // Print the character string.
-//   printf("The file was last modified on %s\n", ctime_str);
-    
-  if (strcmp(ctime_str, str) != 0)
-  {
+
+  if (strcmp(ctime_str, str) != 0) {
     system("npm start");
     strcpy(str, ctime_str);
   }
   sleep(5);
   }
-  
-  
   return 0;
 }
